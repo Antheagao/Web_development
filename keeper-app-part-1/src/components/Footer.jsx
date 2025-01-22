f009226ec33336f69a445c280fa440e5600b8f6f
Antheagao <Anthonymendez035@gmail.com>
2025-01-22 13:11:22 -0800

    removed div from header and footer elements

--- keeper-app-part-1/src/components/Footer.jsx
+++ keeper-app-part-1/src/components/Footer.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 function Footer() {
     return (
-        <div>
-            <footer> 
-                <p> copyright { new Date().getFullYear() } </p> 
-            </footer>
-        </div>
+        <footer> 
+            <p> copyright { new Date().getFullYear() } </p> 
+        </footer>
     );
 }
 
